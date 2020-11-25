4d35825f9abc0ffd0a66d3d06413a0909f805c1a
kevin <kevinfiorentino.1294@gmail.com>
2020-11-24 22:18:41 -0300

    Add primeros testing Jasmine + Coverage istanbul

--- spec/mutation/mutation.spec.js
+++ spec/mutation/mutation.spec.js
@@ -2,9 +2,24 @@
 const { validateDNA } = require('../../models/algorithm');
 
 describe('Testing algorithm', () => {
-    it('Matrix true', () => {
+    it('Matriz correcta x4', () => {
         const arr = ['ATCA', 'AAAA', 'ATCA', 'TGCC'];
         const result = validateDNA(arr);
         expect(result).toBeTruthy();
+    })
+    it('Matriz correcta x6', () => {
+        const arr = ['ATCATT', 'AAAAAA', 'ATCCAA', 'TGCCCA', 'ATCAAC', 'ATAGGC'];
+        const result = validateDNA(arr);
+        expect(result).toBeTruthy();
+    })
+    it('Matriz incorrecta x4', () => {
+        const arr = ['ATCA', 'AAAA', 'ATC', 'TGCC'];
+        const result = validateDNA(arr);
+        expect(result).toBeFalsy();
+    })
+    it('Matriz incorrecta x3', () => {
+        const arr = ['ATCA', 'AAAA', 'ATCA'];
+        const result = validateDNA(arr);
+        expect(result).toBeFalsy();
     })
 })
